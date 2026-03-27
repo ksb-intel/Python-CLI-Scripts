fcabc3c3edf3483c000124d1363e8692dfc4bbb3
ksb-intel <katie.betschart.kb@gmail.com>
2026-03-27 14:20:21 -0500

    practice

---
+++ python-scripts/practice1.py
@@ -0,0 +1,28 @@
+#practice 1
+
+## strings
+text = 'katie'
+print(text)
+
+text = 'KATIE'
+print(text)
+
+text = 'Katie'
+
+## print(text[0:3] = 'KAT')
+len(text)
+print(len(text))
+
+### \n creates a new line
+text = "This is a test \n This is a new line"
+print(text)
+
+## triple quotes does the same thing
+text = """ 
+My name is Katie 
+Welcome to my coding test"""
+
+print(text)
+
+
+
